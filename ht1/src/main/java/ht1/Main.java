d5616d0790136041f247dc6ed655e5e4aa2a01f9
Sofilayerdi <sofilayerdi@gmail.com>
2025-01-27 14:27:40 -0600

    Se corrigio main

--- ht1/src/main/java/ht1/Main.java
+++ ht1/src/main/java/ht1/Main.java
@@ -8,53 +8,55 @@ public class Main {
         Licuadora licuadora = new Licuadora(1, 2.0);
         int op;
 
-        System.out.println("Menu");
-        System.out.println("1. Encender");
-        System.out.println("2. Apagar");
-        System.out.println("3. Llenar");
-        System.out.println("4. Servir");
-        System.out.println("5. Incrementar Velocidad");
-        System.out.println("6. Decrementar Velocidad");
-        System.out.println("7. Vaciar");
-        System.out.println("8. Consultar Estado");
-        System.out.print("Seleccione una opción: ");
-        op = scanner.nextInt();
+        while (true){
+            System.out.println("Menu");
+            System.out.println("1. Encender");
+            System.out.println("2. Apagar");
+            System.out.println("3. Llenar");
+            System.out.println("4. Servir");
+            System.out.println("5. Incrementar Velocidad");
+            System.out.println("6. Decrementar Velocidad");
+            System.out.println("7. Vaciar");
+            System.out.println("8. Consultar Estado");
+            System.out.print("Seleccione una opción: ");
+            op = scanner.nextInt();
 
-        switch (op) {
-            case 1:
-                licuadora.encender();
-                System.out.println("Licuadora encendida.");
-                break;
-            case 2:
-                licuadora.apagar();
-                System.out.println("Licuadora apagada.");
-                break;
-            case 3:
-                System.out.print("Ingrese cantidad a llenar (litros): ");
-                double cantidad = scanner.nextDouble();
-                System.out.println("Cantidad actual: " + licuadora.llenar(cantidad) + " litros");
-                break;
-            case 4:
-                System.out.print("Ingrese cantidad a servir (litros): ");
-                double servir = scanner.nextDouble();
-                System.out.println("Cantidad restante: " + licuadora.servir(servir) + " litros");
+            switch (op) {
+                case 1:
+                    licuadora.encender();
+                    System.out.println("Licuadora encendida.");
                     break;
-            case 5:
-                System.out.println("Velocidad actual: " + licuadora.incrementarVelocidad());
-                break;
-            case 6:
-                System.out.println("Velocidad actual: " + licuadora.decrecerVelocidad());
-                break;
-            case 7:
-                licuadora.vaciar();
-                System.out.println("Licuadora vaciada.");
-                break;
-            case 8:
-                System.out.println("Estado: " + (licuadora.estaLlena() ? "Llena" : "Vacía"));
-                System.out.println("Velocidad: " + licuadora.incrementarVelocidad());
-                break;
-            default:
-                System.out.println("Opción inválida");
+                case 2:
+                    licuadora.apagar();
+                    System.out.println("Licuadora apagada.");
+                    break;
+                case 3:
+                    System.out.print("Ingrese cantidad a llenar (litros): ");
+                    double cantidad = scanner.nextDouble();
+                    System.out.println("Cantidad actual: " + licuadora.llenar(cantidad) + " litros");
+                    break;
+                case 4:
+                    System.out.print("Ingrese cantidad a servir (litros): ");
+                    double servir = scanner.nextDouble();
+                    System.out.println("Cantidad restante: " + licuadora.servir(servir) + " litros");
+                        break;
+                case 5:
+                    System.out.println("Velocidad actual: " + licuadora.incrementarVelocidad());
+                    break;
+                case 6:
+                    System.out.println("Velocidad actual: " + licuadora.decrecerVelocidad());
+                    break;
+                case 7:
+                    licuadora.vaciar();
+                    System.out.println("Licuadora vaciada.");
+                    break;
+                case 8:
+                    System.out.println("Estado: " + (licuadora.estaLlena() ? "Llena" : "Vacía"));
+                    System.out.println("Velocidad: " + licuadora.incrementarVelocidad());
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
         }
     }
 }
